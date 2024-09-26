280334e7305d5c20d9208cdaf559d0f45aac1285
slash <slash@gmail.com>
2024-09-26 14:20:36 +0800

    [RECORD_OFF|RX] add rx-feature RECORD_OFF

--- src/hal_entry.c
+++ src/hal_entry.c
@@ -726,6 +726,11 @@ void cec_rx_data_check(void)
 
         switch(p_buff->opcode)
         {
+            /* One Touch Record Feature */
+            case CEC_OPCODE_RECORD_OFF:
+            {
+                break;
+            }
             case CEC_OPCODE_SET_SYSTEM_AUDIO_MODE:
             {
                 audio_mode_data = p_buff->data_buff[0];
