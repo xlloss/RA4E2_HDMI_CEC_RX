2ee7c5864b7ee99e3cd78fab7d9af72cf3b76a4f
slash <slash@gmail.com>
2024-10-14 10:13:40 +0800

    [CLEAN_CODE] clean cec_interrupt_callback()

--- src/hal_entry.c
+++ src/hal_entry.c
@@ -318,7 +318,10 @@ void cec_interrupt_callback(cec_callback_args_t *p_args)
     switch (p_args->event) {
         case CEC_EVENT_READY:
         {
-            /* In this demo, the ready flag is received by R_CEC_StatusGet(). But application can also get with this interrupt event. */
+            /* In this demo, the ready flag is received by
+             * R_CEC_StatusGet(). But application can also get with
+             * this interrupt event
+             */
             RTT_DEBUG("@@@ READY\r\n");
             break;
         }
@@ -335,7 +338,9 @@ void cec_interrupt_callback(cec_callback_args_t *p_args)
         {
             RTT_DEBUG("@@@ RX 0x%x\r\n", p_args->data_byte);
             /* Application to store and process received data bytes. */
-            cec_rx_message_buff_t* p_buff = &cec_rx_data_buff[cec_rx_data_buff_next_store_point];
+            cec_rx_message_buff_t* p_buff =
+                &cec_rx_data_buff[cec_rx_data_buff_next_store_point];
+
             if (p_buff->byte_counter == 0) {
                 p_buff->source = (uint8_t)(p_args->data_byte >> 4);
                 p_buff->destination = (uint8_t)(p_args->data_byte & 0xF);
@@ -346,7 +351,7 @@ void cec_interrupt_callback(cec_callback_args_t *p_args)
             }
 
             p_buff->byte_counter++;
-            if(p_buff->byte_counter == CEC_DATA_BUFFER_LENGTH) {
+            if (p_buff->byte_counter == CEC_DATA_BUFFER_LENGTH) {
                 p_buff->byte_counter = 0;
             }
             break;
@@ -372,7 +377,9 @@ void cec_interrupt_callback(cec_callback_args_t *p_args)
             cec_err_type = p_args->errors;
 
             if (cec_err_type & (CEC_ERROR_OERR | CEC_ERROR_TERR)) {
-                cec_rx_message_buff_t* p_buff = &cec_rx_data_buff[cec_rx_data_buff_next_store_point];
+                cec_rx_message_buff_t* p_buff =
+                        &cec_rx_data_buff[cec_rx_data_buff_next_store_point];
+
                 if (p_buff->byte_counter > 0) {
                     p_buff->is_error = true;
 
@@ -380,7 +387,7 @@ void cec_interrupt_callback(cec_callback_args_t *p_args)
                     p_buff->is_new_data = true;
                     cec_rx_data_buff_next_store_point++;
                     if (cec_rx_data_buff_next_store_point >=
-                        CEC_RX_DATA_BUFF_DATA_NUMBER) {
+                            CEC_RX_DATA_BUFF_DATA_NUMBER) {
                         cec_rx_data_buff_next_store_point = 0;
                     }
                 }
@@ -389,8 +396,8 @@ void cec_interrupt_callback(cec_callback_args_t *p_args)
             RTT_DEBUG("@@@ ERR 0x%x\r\n", cec_err_type);
             break;
         }
+
         default: {
-            /* Do nothing */
             break;
         }
     }
