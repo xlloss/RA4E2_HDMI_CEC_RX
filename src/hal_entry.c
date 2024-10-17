9c82138ca6451367437958680057cc1a1002d826
slash <slash@gmail.com>
2024-10-17 23:19:14 +0800

    [CEC_EVENT] add standby with event_status

--- src/hal_entry.c
+++ src/hal_entry.c
@@ -1321,15 +1321,11 @@ void cec_rx_data_check(void)
                 cec_ev_package[EV_IMG_VIEW_ON].param_len = 0;
                 break;
             }
-            /* Standby Feature */
 
+            /* Standby Feature */
             case CEC_OPCODE_STANDBY:
             {
-//                cec_action_request_detect_flag = true;
-//                cec_action_type = CEC_ACTION_POWER_OFF;
-//                cec_ev_package[EV_STANDBY].ev_id = EV_STANDBY
-//                cec_ev_package[EV_STANDBY].laddr = p_buff->source;
-//                cec_ev_package[EV_STANDBY].param_len = 0;
+                ev_status_1 |= EV_FG_STANDBY;
                 break;
             }
 
